74cd4e3272839cac917ef9f5bd1d68236fd42d90
25r1116kurata-hash <25r1116.kurata@gmail.com>
2026-03-24 11:01:55 +0900

    3/24 kurata cartT.py、itemM.py、orderD.py、orderH.py、userM.pyの修正

--- EC-site-project/app/model/cartT.py
+++ EC-site-project/app/model/cartT.py
@@ -4,15 +4,15 @@ from sqlalchemy import func
 
 class CartT(db.Model):
     __tablename__ = "cartT"
-    userID = db.Column(db.String(10),primary_key=True) # ユーザーID + 行番号　の複合キー
-    lineNo = db.Column(db.String(30),primary_key=True) # ユーザーID + 行番号　の複合キー
-    itemId = db.Column(db.String(100))
-    amount = db.Column(db.String(100))
-    price = db.Column(db.Float)
-    tax = db.Column(db.Integer)
-    delFlg = db.Column(db.Boolean, default=False, nullable=False)
-    updDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
-    insDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
+    orderId = db.Column(db.String(10),primary_key=True)             # 発注ID 複合キー
+    lineNo = db.Column(db.Integer,primary_key=True)                 # 行番号　複合キー
+    itemId = db.Column(db.String(10))                               # 商品ID
+    amount = db.Column(db.Integer, default=0)                       # 数量
+    price = db.Column(db.Float, default=0)                             # 単価（税抜き）
+    tax = db.Column(db.Float, default=0)                             # 消費税
+    delFlg = db.Column(db.Boolean, default=False, nullable=False)   # 削除フラグ
+    updDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())  # 更新日付
+    insDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())  # 登録日付
 
     # リレーションの設定(子供を指定)
     # 無
@@ -20,7 +20,7 @@ class CartT(db.Model):
     # 辞書型で取得
     def getData(self):
         return{
-            "userID": str(self.userID),
+            "orderId": str(self.orderId),
             "lineNo": int(self.lineNo),
             "itemId": str(self.ItemId),
             "amount": int(self.amount),
--- EC-site-project/app/model/orderH.py
+++ EC-site-project/app/model/orderH.py
@@ -3,18 +3,19 @@ from sqlalchemy import func
 
 class CartT(db.Model):
     __tablename__ = "orderH"
-    orderId = db.Column(db.String(10),primary_key=True) # ユーザーID + 行番号　の複合キー
-    orderDate = db.Column(db.String(10),primary_key=True) # ユーザーID + 行番号　の複合キー
-    userName = db.Column(db.String(50))
-    orderAddress = db.Column(db.String(50))
-    cardNum = db.Column(db.String(50))
-    shipFlg = db.Column(db.Boolean)
-    price = db.Column(db.Float)
-    tax = db.Column(db.Float)
-    total = db.Column(db.Float)
-    delFlg = db.Column(db.Boolean, default=False, nullable=False)
-    updDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
-    insDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())
+    orderId = db.Column(db.String(10),primary_key=True)     # 発注ID 
+    orderDate = db.Column(db.String(10))                    # 注文日付
+    userName = db.Column(db.String(50))                     # お客様名
+    orderAddress = db.Column(db.String(50))                 # お届け先住所
+    cardNum = db.Column(db.String(50))                      # クレジットカード番号
+    shipFlg = db.Column(db.Boolean, default=False)          # 発送フラグ
+    price = db.Column(db.Float, default=0)                  # 小計（税別）
+    tax = db.Column(db.Float, default=0)                    # 消費税
+    shipping  = db.Column(db.Float, default=0)              # 送料
+    total = db.Column(db.Float, default=0)                  # 合計
+    delFlg = db.Column(db.Boolean, default=False, nullable=False)   # 削除フラグ
+    updDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())  # 更新日付
+    insDate = db.Column(db.DateTime(timezone=True), nullable=False, server_default=func.now())  # 登録日付
 
     # リレーションの設定(子供を指定)
     # 無
@@ -22,15 +23,16 @@ class CartT(db.Model):
     # 辞書型で取得
     def getData(self):
         return{
-            "orderId": str(self.userID),
-            "orderDate": str(self.lineNo),
-            "userName": str(self.ItemId),
-            "orderAddress": str(self.amount),
-            "cardNum": str(self.price),
-            "shipFlg": bool(self.tax),
-            "price": float(self.tax),
+            "orderId": str(self.orderId),
+            "orderDate": str(self.orderDate),
+            "userName": str(self.userName),
+            "orderAddress": str(self.orderAddress),
+            "cardNum": str(self.cardNum),
+            "shipFlg": bool(self.shipFlg),
+            "price": float(self.price),
             "tax": float(self.tax),
-            "total": float(self.tax),
+            "shipping": float(self.shipping),
+            "total": float(self.total),
             "delFlg": bool(self.delFlg),
             "updDate": self.updDate.strftime('%Y/%m/%d %H:%M:%S') if self.updDate else None,
             "insDate": self.insDate.strftime('%Y/%m/%d %H:%M:%S') if self.insDate else None
