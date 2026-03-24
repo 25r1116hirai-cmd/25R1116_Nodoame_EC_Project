534b0b3dda9d1f951b45e388ac4b57c48f392397
saori <free.and.else@gmail.com>
2026-03-25 02:29:54 +0900

    商品一覧、商品詳細をアップロード

--- EC-site-project/routes/shop.py
+++ EC-site-project/routes/shop.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, abort, render_template, request
 #DBのテーブルとDB操作のファイルをインポート
 from app.model.itemM import ItemM
 from app.extensions import db
@@ -9,24 +9,40 @@ shop = Blueprint("shop", __name__)
 
 @shop.route('/top')
 def index():
-    # すべて
-    items = ItemM.query.all()
-    # おすすめがTrue、更新日で降順、上限3件分をすべて
-    recommend_items = ItemM.query.filter_by(recmdFlg=True).order_by(ItemM.updDate.desc()).limit(3).all()
 
+    # 共通：論理削除は除く
+    # すべて 
+    all_items = ItemM.query.filter_by(delFlg=False).all()
+    # おすすめ、更新日で降順、上限3件分をすべて
+    recommend_items = ItemM.query.filter_by(recmdFlg=True,delFlg=False).order_by(ItemM.updDate.desc()).limit(3).all()
+    print(all_items)
+    # カテゴリから重複を除去
+    categories = sorted({item.categoryName for item in all_items})
 
+    # カテゴリで絞り込み
+    selected_category = request.args.get("category")
+    if selected_category and selected_category != "all":
+        filtered_items = [item for item in all_items if item.categoryName == selected_category]
+    else:
+        filtered_items = all_items
 
+    # 価格帯で絞り込み（もし price パラメータがある場合）
+    price_filters = {
+    '1000': lambda p: p <= 1000,
+    '3000': lambda p: 1001 <= p <= 3000,
+    '3001': lambda p: p >= 3001,
+}
+    price_range = request.args.get("price") 
+    if price_range in price_filters:
+        filtered_items = [item for item in filtered_items if price_filters[price_range](item.price)]
 
-    # # 価格帯で絞り込み
-    # if price_range:
-    #     if price_range == '1000':
-    #         filtered_products = [p for p in filtered_products if p['price'] <= 1000]
-    #     elif price_range == '3000':
-    #         filtered_products = [p for p in filtered_products if 1001 <= p['price'] <= 3000]
-    #     elif price_range == '3001':
-    #         filtered_products = [p for p in filtered_products if p['price'] >= 3001]
-
-    return render_template("shop/index.html",items=items,reco=recommend_items)
+    return render_template("shop/index.html",
+                           items=filtered_items,
+                           all_items=all_items,
+                           reco=recommend_items,
+                           categories=categories,
+                           selected_category=selected_category
+                          )
 
 @shop.route('/product/<int:id>')
 def detail(id):
@@ -35,6 +51,6 @@ def detail(id):
     if not item:
         abort(404)
 
-    return render_template("shop/detail.html", item=item)
+    return render_template("shop/detail.html", product=item)
     
  
