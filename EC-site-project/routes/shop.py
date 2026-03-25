4a6251db31b9a3b13e41f894d7ab58207b5e9f44
25r1116kimura-ops <25r1116.kimura@gmail.com>
2026-03-25 15:16:00 +0900

    商品詳細ページ内のカート追加処理を追加

--- EC-site-project/routes/shop.py
+++ EC-site-project/routes/shop.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, abort, render_template, request
+from flask import Blueprint, abort, jsonify, redirect, render_template, request, session
 #DBのテーブルとDB操作のファイルをインポート
 from app.model.itemM import ItemM
 from app.extensions import db
@@ -55,4 +55,36 @@ def detail(id):
 
     return render_template("shop/detail.html", product=item)
     
- 
+ # =================================================================================
+# カート追加時の処理(商品一覧ページ、商品詳細ページでカートに追加した際の処理)
+# =================================================================================
+# sessionの中にブラウザ上のcartというデータを入れる箱の中身(商品情報等)を参照させる
+# ここにすでに商品があれば追加で個数を+１する
+# なければ商品の「名前」と「値段」と「個数」を追加する
+# ※名前と値段はデータベース参照
+# 変更後の内容をsessionの中の"cart"の中に登録/更新
+# session.modified = TrueはFlask自身にブラウザの中のcart情報が更新されたことを伝える役割
+# Trueは更新された。Falseは更新されてない。って意味。
+
+# sessionはサイトに来た時点で作成されている(sessionID作成)
+# ブラウザにcookieとして保存される
+# その後の処理はsessionIDに紐づいてcart情報や配送情報が保持される
+@shop.route("/add_to_cart/<int:itemId>", methods=["POST"])
+def add_to_cart(itemId):
+    product = ItemM.query.get(itemId)
+
+    cart = session.get("cart", {})
+
+    if str(itemId) in cart:
+        cart[str(itemId)]["qty"] += 1
+    else:
+        cart[str(itemId)] = {
+            "name": product.name,
+            "price": product.price,
+            "qty": 1
+        }
+
+    session["cart"] = cart
+    session.modified = True
+
+    return jsonify({"message": "カートに追加しました", "total_items": len(session["cart"])})
